e5715481e5064545f1f9b37c5d46424516f2a758
Kenya Chauche <jackiekenya.chauche@gmail.com>
2020-09-10 13:43:06 -0700

    added more notes on data sources, just about ready to start downloading and sourcing

--- data_gathering.py
+++ data_gathering.py
@@ -11,6 +11,10 @@
 # Senator and Representative dataset
 # https://github.com/unitedstates/congress-legislators
 # repo containing csv of current representatives from @unitedstates
+# gender
+# age
+# party
+# ** remove rows with state abbrevations PR (Puerto Rico), AS (American Samoa), GU (Guam)
 
 # Senate religious alignments
 # https://en.wikipedia.org/wiki/Religious_affiliation_in_the_United_States_Senate
@@ -19,6 +23,7 @@
 # https://en.wikipedia.org/wiki/Religious_affiliation_in_the_United_States_House_of_Representatives
 
 # wikipedia API
+# education level
 # https://pypi.org/project/Wikipedia-API/
 
 # state demographics
@@ -28,3 +33,15 @@
 # https://www.pewforum.org/religious-landscape-study/
 # education level
 # https://data.census.gov/cedsci/table?q=educational%20attainment&t=Educational%20Attainment&g=0100000US.04000.001&y=2018&tid=ACSST1Y2018.S1501&moe=false&tp=true&hidePreview=true
+# party affiliation
+# if all else fails: https://www.pewforum.org/religious-landscape-study/compare/party-affiliation/by/state/
+# age distribution
+#
+
+# stretch: representative voting record
+# sponsor and cosponsoring bills
+# https://www.congress.gov/member/ralph-abraham/A000374?searchResultViewType=expanded (replace with name and bioguide id)
+# voting record
+# https://clerk.house.gov/members/Y000033 (replace with bioguide id)
+# stretch: website
+# in @unitedstates dataset
